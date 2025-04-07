c2f857db6ccf26c9ae7499c749620d8269b9d515
JeongHoon <100833901+Hoon1999@users.noreply.github.com>
2025-04-08 00:37:40 +0900

    feat: toc 클릭시 스크롤 되는 기능 추가

--- js/toc.js
+++ js/toc.js
@@ -6,8 +6,20 @@ function createToC() {
     const toc_list = document.querySelector('#toc-list');
 
     tags.forEach((tag) => {
+        let a = document.createElement('a');
         let li = document.createElement('li');
+
+        // toc 요소를 클릭하면 해당 content 의 위치로 스크롤 되는 이벤트.
+        a.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            let t = tag.getBoundingClientRect().top // 해당 h3 태그의 top 좌표를 가져옴.
+
+            window.scrollTo({ top: t, behavior: 'smooth' }) // 해당 좌표의 위치로 스크롤된다.
+        })
         li.innerText = tag.textContent; // h3 의 innerText를 가져와 할당한다.
-        toc_list.appendChild(li);
+
+        a.appendChild(li);
+        toc_list.appendChild(a);
     })
 }
